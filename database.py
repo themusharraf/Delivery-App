493e4dfd3db88e771ec709fe946741aea8c3df1e
msi <meibrohimov.@gmail.com>
2024-04-20 21:44:43 +0500

    ADD database file 👀

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "'postgresql://posttest:1@localhost/appdb'"
+DATABASE_URL = "postgresql://posttest:1@localhost/appdb"
 engine = create_engine(DATABASE_URL, echo=True)  # database log echo = True
 
 Base = declarative_base()
--- init_db.py
+++ init_db.py
@@ -1,1 +1,6 @@
-from database import Base,engine
+from database import Base, engine
+from models.models import User, Product, Order
+
+Base.metadata.create_all(engine)
+
+# python3 init_db.py // database create table command
